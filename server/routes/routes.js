01ece78116844df110484cadb05ec7075cc1c08e
neji25 <44751599+neji25@users.noreply.github.com>
2018-11-05 22:15:00 +0300

    Смена имени основной модели

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var Expense = require('../../models/Expense');
+var Expense = require('../../models/device');
 var Passport = require('../../models/passport');
 router.get('/', function(req, res){
   res.render('index')
