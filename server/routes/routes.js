3bcc8d056ca48d4fc5524e8c705abcf84729ef14
neji25 <44751599+neji25@users.noreply.github.com>
2018-11-18 13:19:26 +0300

    Some changes in routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -12,6 +12,11 @@ router.get('/', function(req, res){
 });
 router.route('/insert')
 .post(function(req,res) {
+
+  var passport = new Passport();
+  var verification = new Verification();
+  var repair = new Repair();
+
  var device = new Device({
    title: req.body.title,
    type: req.body.type,
@@ -20,50 +25,53 @@ router.route('/insert')
    scope: req.body.scope,
    location: req.body.location,
    status: req.body.status,
-   comment: req.body.comment
+   comment: req.body.comment,
+   passport: passport._id,
+   verification: verification._id,
+   repair: repair._id
  });
 device.save(function(err) {
       if (err)
         res.send(err);
-      var passport = new Passport({
-        device: device._id,
-        serial_number: req.body.serial_number,
-        passport_number: req.body.passport_number,
-        inventory_number: req.body.inventory_number,
-        stock_number: req.body.stock_number,
-        maker: req.body.maker,
-        barcode: req.body.barcode,
-        initial_cost: req.body.initial_cost,
-        construction_date: req.body.construction_date,
-        start_date: req.body.start_date,
-        change_data: req.body.change_data
-      });
-      var verification = new Verification({
-        device: device._id,
-        last_verif_date: req.body.last_verif_date,
-        verif_period: req.body.verif_period,
-        next_verif_date: req.body.next_verif_date,
-        left_until: req.body.left_until,
-        verif_cost: req.body.verif_cost,
-        verif_location: req.body.verif_location,
-        verif_unit_code: req.body.verif_unit_code,
-        verif_type: req.body.verif_type,
-        verif_metodology: req.body.verif_metodology,
-        verif_officer_name: req.body.verif_officer_name,
-        conclusion: req.body.conclusion,
-        report_number: req.body.report_number,
-        mark_type: req.body.mark_type,
-        mark_number: req.body.mark_number
-      });
-      var repair = new Repair({
-        device: device._id,
-        reason_for_repair: req.body.reason_for_repair,
-        repair_date: req.body.repair_date,
-        repair_address: req.body.repair_address,
-        repair_cost: req.body.repair_cost,
-        short_description: req.body.short_description
-      });
+
+        passport.device = device._id;
+        passport.serial_number = req.body.serial_number;
+        passport.passport_number = req.body.passport_number;
+        passport.inventory_number = req.body.inventory_number;
+        passport.stock_number = req.body.stock_number;
+        passport.maker = req.body.maker;
+        passport.barcode = req.body.barcode;
+        passport.initial_cost = req.body.initial_cost;
+        passport.construction_date = req.body.construction_date;
+        passport.start_date = req.body.start_date;
+        passport.change_data = req.body.change_date;
       
+      
+        verification.device = device._id,
+        verification.last_verif_date = req.body.last_verif_date,
+        verification.verif_period = req.body.verif_period,
+        verification.next_verif_date = req.body.next_verif_date,
+        verification.left_until = req.body.left_until,
+        verification.verif_cost = req.body.verif_cost,
+        verification.verif_location = req.body.verif_location,
+        verification.verif_unit_code = req.body.verif_unit_code,
+        verification.verif_type = req.body.verif_type,
+        verification.verif_metodology = req.body.verif_metodology,
+        verification.verif_officer_name = req.body.verif_officer_name,
+        verification.conclusion = req.body.conclusion,
+        verification.report_number = req.body.report_number,
+        verification.mark_type = req.body.mark_type,
+        verification.mark_number = req.body.mark_number
+      
+      
+        repair.device = device._id
+        repair.reason_for_repair = req.body.reason_for_repair
+        repair.repair_date = req.body.repair_date
+        repair.repair_address = req.body.repair_address
+        repair.repair_cost = req.body.repair_cost
+        repair.short_description = req.body.short_description
+      
+
       passport.save(function(err) {
         if(err) res.send(err);
       })
@@ -102,7 +110,7 @@ router.get('/delete', function(req, res){
 });
 router.get('/getAll', function(req, res) {
  
- Expense.find().exec(function(err, so) {
+ Device.find({}).populate(['passport', 'verification', 'repair']).exec(function(err, so) {
    if(err)
     res.send(err);
     res.json(so);
